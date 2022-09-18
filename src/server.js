a0173c2f90cd44cd32a2f26cc470395fd00ecabb
lindennerd <lindennerd@gmail.com>
2022-09-18 10:29:47 -0300

    fix: flattening router

--- src/server.js
+++ src/server.js
@@ -4,20 +4,32 @@ const serverless = require('serverless-http');
 const swaggerUi = require('swagger-ui-express');
 const path = require('path');
 const swaggerFile = require('../swagger-output.json')
-const imageRouter = require('./routes/image');
+const controller = require('./controller/imageController');
+
 const morgan = require('morgan');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+
+router.get('/image/:imageUrl/:text', controller.getImage);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 if (process.env.NODE_ENV === 'production') {
-    app.use('/.netlify/functions/server/', imageRouter);
+    app.use('/.netlify/functions/server', router);
 } else {
-    app.use('/', imageRouter);
+    app.use('/', router);
 }
 
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
