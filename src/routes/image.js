c2b38e57643b58ff396dffc65dd2048b646a9986
lindennerd <lindennerd@gmail.com>
2022-09-18 09:59:29 -0300

    fix: add doc generation to toml

--- src/routes/image.js
+++ src/routes/image.js
@@ -3,9 +3,6 @@ const controller = require('../controller/imageController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Up and Running!');
-});
 router.get('image/:imageUrl/:text', controller.getImage);
 
 module.exports = router;
