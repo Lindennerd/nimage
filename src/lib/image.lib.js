cb8155a4384021d2f4a38bcfcb3988c1e92d9313
lindennerd <lindennerd@gmail.com>
2022-09-18 17:39:38 -0300

    fix: fixing the other functions affected by the bug

--- src/lib/image.lib.js
+++ src/lib/image.lib.js
@@ -9,10 +9,17 @@ const imageLib = () => {
 
     //REF: https://github.com/Vibrant-Colors/node-vibrant/issues/108
     // apparently the latest version of jimp doesn't work very well when compiled
+    const jimpFont = jimp.FONT_SANS_32_BLACK
+      ? jimp.FONT_SANS_32_BLACK
+      : jimp.default.FONT_SANS_32_BLACK;
+
     const image = await (jimp.read
       ? jimp.read(imageUrl)
       : jimp.default.read(imageUrl));
-    const loadedFont = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+
+    const loadedFont = await (jimp.loadFont
+      ? jimp.loadFont(jimpFont)
+      : jimp.loadFont(jimpFont));
 
     const printedImage = await image.print(loadedFont, 10, 90, text, 700, 100);
 
