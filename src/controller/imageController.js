a8e21352b7d3b4e7c1308dcb02eb697ed650438f
lindennerd <lindennerd@gmail.com>
2022-09-18 12:10:59 -0300

    debugging production

--- src/controller/imageController.js
+++ src/controller/imageController.js
@@ -9,10 +9,15 @@ const validateURL = (url) => {
 module.exports = {
     async getImage(req, res, next) {
         const { imageUrl, text } = req.params;
+        console.log("Request Received!");
+        console.log("ImageUrl", imageUrl);
+        console.log("Text", text);
 
         if ((!validateURL(imageUrl) || !imageUrl )|| !text) res.status(400).send('Invalid params');
-
-        const image = await imageLib().pasteTextToImage(imageUrl, text);
-        res.send(image);
+        else {
+            console.log("Request validated");
+            const image = await imageLib().pasteTextToImage(imageUrl, text);
+            res.send(image);
+        }
     }
 };
--- src/lib/image.lib.js
+++ src/lib/image.lib.js
@@ -2,9 +2,14 @@ const jimp = require("jimp");
 
 const imageLib = () => {
   const pasteTextToImage = async (imageUrl, text) => {
+    console.log("loading image into jimp");
     const image = await jimp.read(imageUrl);
+    console.log("loading fonts for jimp");
     const loadedFont = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+    console.log("printing text into image");
+
     const printedImage = await image.print(loadedFont, 10, 90, text, 700, 100);
+    console.log("image printed successfully");
     return await printedImage.getBase64Async("image/jpeg");
   };
 
