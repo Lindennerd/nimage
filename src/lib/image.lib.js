d664f43b2bd005319fde24e29f4fbc2212caa758
lindennerd <lindennerd@gmail.com>
2022-09-18 18:04:55 -0300

    try: trying to fix the font location bug

--- src/lib/image.lib.js
+++ src/lib/image.lib.js
@@ -1,18 +1,16 @@
 const jimp = require("jimp");
+const pluginPrint = require("@jimp/plugin-print");
 
 const imageLib = () => {
   const pasteTextToImage = async (imageUrl, text) => {
     //REF: https://github.com/Vibrant-Colors/node-vibrant/issues/108
     // apparently the latest version of jimp doesn't work very well when compiled
-    const jimpFont = jimp.FONT_SANS_32_BLACK
-      ? jimp.FONT_SANS_32_BLACK
-      : jimp.default.FONT_SANS_32_BLACK;
+
+    const jimpFont = pluginPrint().constants.FONT_SANS_32_BLACK;
 
     const image = await (jimp.read
       ? jimp.read(imageUrl)
       : jimp.default.read(imageUrl));
-
-    console.log("Jimp loadFont function", jimp.default.loadFont);
 
     const loadedFont = await (jimp.loadFont
       ? jimp.loadFont(jimpFont)
