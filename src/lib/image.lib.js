b2e0933abb07409220e71fd3e07f87b0953306a3
lindennerd <lindennerd@gmail.com>
2022-09-18 22:05:23 -0300

    debug: prod image generation

--- src/lib/image.lib.js
+++ src/lib/image.lib.js
@@ -6,17 +6,25 @@ const imageLib = () => {
     //REF: https://github.com/Vibrant-Colors/node-vibrant/issues/108
     // apparently the latest version of jimp doesn't work very well when compiled
 
+    console.log("getting plugin");
+
     const jimpFont = pluginPrint().constants.FONT_SANS_32_BLACK;
 
     const image = await (jimp.read
       ? jimp.read(imageUrl)
       : jimp.default.read(imageUrl));
 
+    console.log("loading font");
+
     const loadedFont = await (jimp.loadFont
       ? jimp.loadFont(jimpFont)
       : jimp.default.loadFont(jimpFont));
 
+    console.log("printing image");
+
     const printedImage = await image.print(loadedFont, 10, 90, text, 700, 100);
+
+    console.log("image printed");
 
     return await printedImage.getBase64Async("image/jpeg");
   };
